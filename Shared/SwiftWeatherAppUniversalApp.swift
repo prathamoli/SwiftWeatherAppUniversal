5520ee9c64ac3a23b9c487290f7e880d7865fcb9
Pratham Oli <oli.pratham56@icloud.com>
2022-07-29 12:44:07 +0545

    Initial Commit

---
+++ Shared/SwiftWeatherAppUniversalApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftWeatherAppUniversalApp.swift
+//  Shared
+//
+//  Created by Thalle on 29/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftWeatherAppUniversalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
